7c92ab2754d9ebd00b06e6e6ec6caa534df0151c
Joe Miceli <joe.miceli8@gmail.com>
2023-08-12 22:46:09 -0600

    moved config parsing to be a standalone module, updated print statements in analysis scrit

--- sumo_analysis.py
+++ sumo_analysis.py
@@ -14,15 +14,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import configargparse
-from distutils.util import strtobool
 import numpy as np
 import datetime
 
-import random
 import os
-import csv
-import pettingzoo
 
 # SUMO dependencies
 import sumo_rl
@@ -30,6 +25,9 @@ import sys
 
 from sumo_custom_observation import CustomObservationFunction
 from sumo_custom_reward import MaxSpeedRewardFunction
+
+# Config Parser
+from MARLConfigParser import MARLConfigParser
 
 # Make sure SUMO env variable is set
 if 'SUMO_HOME' in os.environ:
@@ -63,91 +61,9 @@ class QNetwork(nn.Module):
 
 
 if __name__ == "__main__":
-    parser = configargparse.ArgParser(default_config_files=['experiments/sumo-4x4-independent.config'], description='DQN agent')
-    parser.add_argument('-c', '--config_path', required=False, is_config_file=True, help='config file path')
-
-    # TODO: remove unecessary configs here, we're just looking at sumo in this file
-    # Common arguments
-    parser.add_argument('--exp-name', type=str, default=os.path.basename(__file__).rstrip(".py"),
-                        help='the name of this experiment')
-    parser.add_argument('--gym-id', type=str, default="CartPole-v0",
-                        help='the id of the gym environment')
-    parser.add_argument('--env-args', type=str, default="",
-                        help='string to pass to env init')
-    parser.add_argument('--learning-rate', type=float, default=7e-4,
-                        help='the learning rate of the optimizer')
-    parser.add_argument('--seed', type=int, default=1,
-                        help='seed of the experiment')
-    parser.add_argument('--total-timesteps', type=int, default=500000,
-                        help='total timesteps of the experiments')
-    parser.add_argument('--max-cycles', type=int, default=100,
-                        help='max cycles in each step of the experiments')
-    parser.add_argument('--torch-deterministic', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
-                        help='if toggled, `torch.backends.cudnn.deterministic=False`')
-    parser.add_argument('--cuda', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
-                        help='if toggled, cuda will not be enabled by default')
-    parser.add_argument('--prod-mode', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
-                        help='run the script in production mode and use wandb to log outputs')
-    parser.add_argument('--capture-video', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
-                        help='weather to capture videos of the agent performances (check out `videos` folder)')
-    parser.add_argument('--wandb-project-name', type=str, default="DA-RL",
-                        help="the wandb's project name")
-    parser.add_argument('--wandb-entity', type=str, default=None,
-                        help="the entity (team) of wandb's project")
-    parser.add_argument('--render', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
-                        help='if toggled, render environment')
-    parser.add_argument('--global-obs', type=lambda x:bool(strtobool(x)), default=False, nargs='?', const=True,
-                        help='if toggled, stack agent observations into global state')
-    parser.add_argument('--gpu-id', type=str, default=None,
-                        help='gpu device to use')
-    parser.add_argument('--nn-save-freq', type=int, default=1000,
-                        help='how often to save a copy of the neural network')
-
-    # Algorithm specific arguments
-    parser.add_argument('--N', type=int, default=3,
-                        help='the number of agents')
-    parser.add_argument('--buffer-size', type=int, default=10000,
-                         help='the replay memory buffer size')
-    parser.add_argument('--gamma', type=float, default=0.99,
-                        help='the discount factor gamma')
-    parser.add_argument('--target-network-frequency', type=int, default=500,
-                        help="the timesteps it takes to update the target network")
-    parser.add_argument('--max-grad-norm', type=float, default=0.5,
-                        help='the maximum norm for the gradient clipping')
-    parser.add_argument('--batch-size', type=int, default=32,
-                        help="the batch size of sample from the reply memory")
-    parser.add_argument('--start-e', type=float, default=1,
-                        help="the starting epsilon for exploration")
-    parser.add_argument('--end-e', type=float, default=0.05,
-                        help="the ending epsilon for exploration")
-    parser.add_argument('--lam', type=float, default=0.01,
-                        help="the pension for the variance")
-    parser.add_argument('--exploration-fraction', type=float, default=0.05,
-                        help="the fraction of `total-timesteps` it takes from start-e to go end-e")
-    parser.add_argument('--learning-starts', type=int, default=10000,
-                        help="timestep to start learning")
-    parser.add_argument('--train-frequency', type=int, default=1,
-                        help="the frequency of training")
-    parser.add_argument('--load-weights', type=bool, default=False,
-                    help="whether to load weights for the Q Network")
-
-    # Configuration parameters specific to the SUMO traffic environment
-    parser.add_argument("--route-file", dest="route", type=str, required=False, help="Route definition xml file.\n")
-    parser.add_argument("--net-file", dest="net", type=str, required=False, help="Net definition xml file.\n")
-    parser.add_argument("--mingreen", dest="min_green", type=int, default=10, required=False, help="Minimum time for green lights in SUMO environment.\n")
-    parser.add_argument("--maxgreen", dest="max_green", type=int, default=30, required=False, help="Maximum time for green lights in SUMO environment.\n")
-    parser.add_argument("--sumo-gui", dest="sumo_gui", action="store_true", default=False, help="Run with visualization on SUMO (may require firewall permissions).\n")
-    parser.add_argument("--sumo-seconds", dest="sumo_seconds", type=int, default=10000, required=False, help="Number of simulation seconds. The number of seconds the simulation must end.\n")
-    parser.add_argument("--sumo-reward", dest="sumo_reward", type=str, default='wait', required=False, help="Reward function: \nThe 'queue'reward returns the negative number of total vehicles stopped at all agents each step, \nThe 'wait' reward returns the negative number of cummulative seconds that vehicles have been waiting in the episode.\n")
-
-    # Configuration parameters for analyzing sumo env
-    parser.add_argument("--analysis-steps", dest="analysis_steps", type=int, default=500, required=True, 
-                        help="The number of time steps at which we want to investigate the perfomance of the algorithm. E.g. display how the training was going at the 10,000 checkpoint. Note there must be a nn .pt file for each agent at this step.\n")
-    parser.add_argument("--nn-directory", dest="nn_directory", type=str, default=None, required=True, 
-                        help="The directory containing the nn .pt files to load for analysis.\n")
-    parser.add_argument("--parameter-sharing-model", dest="parameter_sharing_model", action="store_true", default=False, required=False, 
-                        help="Flag indicating if the model trained leveraged parameter sharing or not (needed to identify the size of the model to load).\n")
-                        
+    
+    # Get config parameters                        
+    parser = MARLConfigParser()
     args = parser.parse_args()
     
     if not args.seed:
@@ -169,7 +85,7 @@ if __name__ == "__main__":
     # The 'queue' reward is being used here which returns the (negative) total number of vehicles stopped at all intersections
     if (args.sumo_reward == "custom"):
         # Use the custom "max speed" reward function
-        print ( " > Using CUSTOM reward")
+        print ( " > Evaluating model using CUSTOM reward")
         env = sumo_rl.parallel_env(net_file=args.net, 
                                 route_file=args.route,
                                 use_gui=args.sumo_gui,
@@ -180,7 +96,7 @@ if __name__ == "__main__":
                                 observation_class=CustomObservationFunction,
                                 sumo_warnings=False)
     else:
-        print ( " > Using standard reward")
+        print ( " > Evaluating model using standard reward: {}".format(args.sumo_reward))
         # The 'queue' reward is being used here which returns the (negative) total number of vehicles stopped at all intersections
         env = sumo_rl.parallel_env(net_file=args.net, 
                                 route_file=args.route,
