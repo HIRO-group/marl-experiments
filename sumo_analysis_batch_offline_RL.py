added scenarios for coordinated agent analyses, fixed bug in evaluation of average speed metric so that it is properly recognizing when no cars are present in the intersection (and returning 0 if that is the case), added new config arg to support multiple asl models for evaluation

sumo_analysis_batch_offline_RL.py
@@ -3,7 +3,13 @@ sumo_analysis_batch_offline_RL.py
 
 Description:
     Script for analyzing the policies produced by the the batch offline RL routine
-    NOTE: This file currently assumes that the 3x3 sumo configuration (i.e. 9 agents) is being loaded
+    NOTE: This file currently assumes that the 3x3 sumo configuration (i.e. 9 agents) is being loaded 
+    If parameter sharing is being used, this file is assuming that and multiple models are being evaluated 
+    (specified with nn-queue-directory, nn-speed-overage-directory, and nn-speed-overage-directory-2 parameters)
+    If parameter sharing is NOT being used, this file uses an AGENT_ALIAS_MAP to map agents trained on a 2x2 env
+    and applies them to the 3x3 env
+
+    Eventually these assumptions should be abstracted away to the config file
 
 Usage:
     python sumo_analysis_batch_offline_RL.py -c experiments/sumo-3x3.config
@@ -38,28 +44,29 @@ else:
     sys.exit("Please declare the environment variable 'SUMO_HOME'")
 
 
-# Hard coded to map agents trained on the 2x2.net.xml and 2x2.rou.xml SUMO configuration
-# to agents in the 3x3.net.xml and 3x3Grid2lanes.rou.xml SUMO configuration
-# Keys are the agents for the 4x4 env and values are the "trained" agents from the 2x2 env
-
-# Adding a more programatic way to map policies between envs may not be feasible.. 
-AGENT_ALIAS_MAP = { '0':'1',
-                    '1':'2',
-                    '2':'1',
-                    '3':'5',
-                    '4':'6',
-                    '5':'5',
-                    '6':'1',
-                    '7':'2',
-                    '8':'1'}
-
 if __name__ == "__main__":
     
     # Get config parameters                        
     parser = MARLConfigParser()
     args = parser.parse_args()
-    
-    # QUEUE BASELINE
+
+    # TODO: This should be moved to config
+    # This is a hardcoded mapping of agents trained on the 2x2.net.xml and 2x2.rou.xml SUMO configuration
+    # to agents in the 3x3.net.xml and 3x3Grid2lanes.rou.xml SUMO configuration
+    # Keys are the agents for the 4x4 env and values are the "trained" agents from the 2x2 env
+    # This is used for non-parameter sharing models
+    AGENT_ALIAS_MAP = { '0':'1',
+                        '1':'2',
+                        '2':'1',
+                        '3':'5',
+                        '4':'6',
+                        '5':'5',
+                        '6':'1',
+                        '7':'2',
+                        '8':'1'}
+
+    # TODO: these need to move to config once json is implemented
+    # # QUEUE BASELINE
     # agent_policy_map = {'0': args.nn_queue_directory,
     #                     '1': args.nn_queue_directory,
     #                     '2': args.nn_queue_directory,
@@ -69,19 +76,193 @@ if __name__ == "__main__":
     #                     '6': args.nn_queue_directory,
     #                     '7': args.nn_queue_directory,
     #                     '8': args.nn_queue_directory}
+
+    # # ASL7 Baseline
+    # agent_policy_map = {'0': args.nn_speed_overage_directory,
+    #                     '1': args.nn_speed_overage_directory,
+    #                     '2': args.nn_speed_overage_directory,
+    #                     '3': args.nn_speed_overage_directory,
+    #                     '4': args.nn_speed_overage_directory,
+    #                     '5': args.nn_speed_overage_directory,
+    #                     '6': args.nn_speed_overage_directory,
+    #                     '7': args.nn_speed_overage_directory,
+    #                     '8': args.nn_speed_overage_directory}
     
-    # TODO: these need to move to config once json is implemented
-    # TODO: add another arg for asl7/asl10 difference, right now we're only able to upload one policy
-    # SCENARIO 1 & 2
-    agent_policy_map = {'0': args.nn_speed_overage_directory,
-                        '1': args.nn_speed_overage_directory,
+    # # ASL10 Baseline
+    # agent_policy_map = {'0': args.nn_speed_overage_directory_2,
+    #                     '1': args.nn_speed_overage_directory_2,
+    #                     '2': args.nn_speed_overage_directory_2,
+    #                     '3': args.nn_speed_overage_directory_2,
+    #                     '4': args.nn_speed_overage_directory_2,
+    #                     '5': args.nn_speed_overage_directory_2,
+    #                     '6': args.nn_speed_overage_directory_2,
+    #                     '7': args.nn_speed_overage_directory_2,
+    #                     '8': args.nn_speed_overage_directory_2}
+
+    # SCENARIO 1 
+    # agent_policy_map = {'0': args.nn_speed_overage_directory,
+    #                     '1': args.nn_speed_overage_directory,
+    #                     '2': args.nn_speed_overage_directory,
+    #                     '3': args.nn_speed_overage_directory,
+    #                     '4': args.nn_queue_directory,
+    #                     '5': args.nn_speed_overage_directory,
+    #                     '6': args.nn_speed_overage_directory,
+    #                     '7': args.nn_speed_overage_directory,
+    #                     '8': args.nn_speed_overage_directory}
+
+    # # Scenario 2
+    # agent_policy_map = {'0': args.nn_speed_overage_directory_2,
+    #                     '1': args.nn_speed_overage_directory_2,
+    #                     '2': args.nn_speed_overage_directory_2,
+    #                     '3': args.nn_speed_overage_directory_2,
+    #                     '4': args.nn_queue_directory,
+    #                     '5': args.nn_speed_overage_directory_2,
+    #                     '6': args.nn_speed_overage_directory_2,
+    #                     '7': args.nn_speed_overage_directory_2,
+    #                     '8': args.nn_speed_overage_directory_2}
+
+    # # Scenario 3
+    # agent_policy_map = {'0': args.nn_speed_overage_directory,
+    #                     '1': args.nn_speed_overage_directory_2,
+    #                     '2': args.nn_speed_overage_directory,
+    #                     '3': args.nn_speed_overage_directory_2,
+    #                     '4': args.nn_queue_directory,
+    #                     '5': args.nn_speed_overage_directory_2,
+    #                     '6': args.nn_speed_overage_directory,
+    #                     '7': args.nn_speed_overage_directory_2,
+    #                     '8': args.nn_speed_overage_directory}
+    # # Scenario 4
+    # agent_policy_map = {'0': args.nn_speed_overage_directory,
+    #                     '1': args.nn_speed_overage_directory,
+    #                     '2': args.nn_speed_overage_directory_2,
+    #                     '3': args.nn_speed_overage_directory_2,
+    #                     '4': args.nn_queue_directory,
+    #                     '5': args.nn_speed_overage_directory_2,
+    #                     '6': args.nn_speed_overage_directory_2,
+    #                     '7': args.nn_speed_overage_directory,
+    #                     '8': args.nn_speed_overage_directory}
+
+    # # Scenario 5
+    # agent_policy_map = {'0': args.nn_speed_overage_directory_2,
+    #                     '1': args.nn_speed_overage_directory,
+    #                     '2': args.nn_speed_overage_directory_2,
+    #                     '3': args.nn_speed_overage_directory,
+    #                     '4': args.nn_queue_directory,
+    #                     '5': args.nn_speed_overage_directory,
+    #                     '6': args.nn_speed_overage_directory_2,
+    #                     '7': args.nn_speed_overage_directory,
+    #                     '8': args.nn_speed_overage_directory_2}
+
+    # # Scenario 6
+    # agent_policy_map = {'0': args.nn_speed_overage_directory_2,
+    #                     '1': args.nn_speed_overage_directory_2,
+    #                     '2': args.nn_speed_overage_directory_2,
+    #                     '3': args.nn_speed_overage_directory_2,
+    #                     '4': args.nn_speed_overage_directory,
+    #                     '5': args.nn_speed_overage_directory_2,
+    #                     '6': args.nn_speed_overage_directory_2,
+    #                     '7': args.nn_speed_overage_directory_2,
+    #                     '8': args.nn_speed_overage_directory_2}
+
+    # # Scenario 7
+    # agent_policy_map = {'0': args.nn_queue_directory,
+    #                     '1': args.nn_queue_directory,
+    #                     '2': args.nn_queue_directory,
+    #                     '3': args.nn_queue_directory,
+    #                     '4': args.nn_speed_overage_directory,
+    #                     '5': args.nn_queue_directory,
+    #                     '6': args.nn_queue_directory,
+    #                     '7': args.nn_queue_directory,
+    #                     '8': args.nn_queue_directory}
+
+    # # Scenario 8
+    # agent_policy_map = {'0': args.nn_speed_overage_directory_2,
+    #                     '1': args.nn_queue_directory,
+    #                     '2': args.nn_speed_overage_directory_2,
+    #                     '3': args.nn_queue_directory,
+    #                     '4': args.nn_speed_overage_directory,
+    #                     '5': args.nn_queue_directory,
+    #                     '6': args.nn_speed_overage_directory_2,
+    #                     '7': args.nn_queue_directory,
+    #                     '8': args.nn_speed_overage_directory_2}
+
+    # # Scenario 9
+    # agent_policy_map = {'0': args.nn_queue_directory,
+    #                     '1': args.nn_speed_overage_directory_2,
+    #                     '2': args.nn_queue_directory,
+    #                     '3': args.nn_speed_overage_directory_2,
+    #                     '4': args.nn_speed_overage_directory,
+    #                     '5': args.nn_speed_overage_directory_2,
+    #                     '6': args.nn_queue_directory,
+    #                     '7': args.nn_speed_overage_directory_2,
+    #                     '8': args.nn_queue_directory}
+
+    # # Scenario 10
+    # agent_policy_map = {'0': args.nn_queue_directory,
+    #                     '1': args.nn_queue_directory,
+    #                     '2': args.nn_speed_overage_directory_2,
+    #                     '3': args.nn_speed_overage_directory_2,
+    #                     '4': args.nn_speed_overage_directory,
+    #                     '5': args.nn_speed_overage_directory_2,
+    #                     '6': args.nn_speed_overage_directory_2,
+    #                     '7': args.nn_queue_directory,
+    #                     '8': args.nn_queue_directory}
+
+    # # Scenario 11
+    # agent_policy_map = {'0': args.nn_queue_directory,
+    #                     '1': args.nn_queue_directory,
+    #                     '2': args.nn_queue_directory,
+    #                     '3': args.nn_queue_directory,
+    #                     '4': args.nn_speed_overage_directory_2,
+    #                     '5': args.nn_queue_directory,
+    #                     '6': args.nn_queue_directory,
+    #                     '7': args.nn_queue_directory,
+    #                     '8': args.nn_queue_directory}
+
+    # # Scenario 12
+    # agent_policy_map = {'0': args.nn_speed_overage_directory,
+    #                     '1': args.nn_speed_overage_directory,
+    #                     '2': args.nn_speed_overage_directory,
+    #                     '3': args.nn_speed_overage_directory,
+    #                     '4': args.nn_speed_overage_directory_2,
+    #                     '5': args.nn_speed_overage_directory,
+    #                     '6': args.nn_speed_overage_directory,
+    #                     '7': args.nn_speed_overage_directory,
+    #                     '8': args.nn_speed_overage_directory}
+    
+    # # Scenario 13
+    # agent_policy_map = {'0': args.nn_queue_directory,
+    #                     '1': args.nn_speed_overage_directory,
+    #                     '2': args.nn_queue_directory,
+    #                     '3': args.nn_speed_overage_directory,
+    #                     '4': args.nn_speed_overage_directory_2,
+    #                     '5': args.nn_speed_overage_directory,
+    #                     '6': args.nn_queue_directory,
+    #                     '7': args.nn_speed_overage_directory,
+    #                     '8': args.nn_queue_directory}
+
+    # # Scenario 14
+    # agent_policy_map = {'0': args.nn_speed_overage_directory,
+    #                     '1': args.nn_queue_directory,
+    #                     '2': args.nn_speed_overage_directory,
+    #                     '3': args.nn_queue_directory,
+    #                     '4': args.nn_speed_overage_directory_2,
+    #                     '5': args.nn_queue_directory,
+    #                     '6': args.nn_speed_overage_directory,
+    #                     '7': args.nn_queue_directory,
+    #                     '8': args.nn_speed_overage_directory}
+
+    # Scenario 15
+    agent_policy_map = {'0': args.nn_queue_directory,
+                        '1': args.nn_queue_directory,
                         '2': args.nn_speed_overage_directory,
                         '3': args.nn_speed_overage_directory,
-                        '4': args.nn_queue_directory,
+                        '4': args.nn_speed_overage_directory_2,
                         '5': args.nn_speed_overage_directory,
                         '6': args.nn_speed_overage_directory,
-                        '7': args.nn_speed_overage_directory,
-                        '8': args.nn_speed_overage_directory}
+                        '7': args.nn_queue_directory,
+                        '8': args.nn_queue_directory}
+
 
     # The limit used to evaluate avg speed error (the g1 metric)
     SPEED_LIMIT = args.sumo_average_speed_limit
@@ -121,7 +302,6 @@ if __name__ == "__main__":
                             max_green=args.max_green,
                             min_green=args.min_green,
                             num_seconds=args.sumo_seconds,
-                            delta_time=5,
                             add_system_info=True,       # Default is True
                             add_per_agent_info=True,    # Default is True                                       
                             reward_fn=sumo_reward_function,
@@ -133,7 +313,7 @@ if __name__ == "__main__":
     action_spaces = env.action_spaces
     observation_spaces = env.observation_spaces
     onehot_keys = {agent: i for i, agent in enumerate(agents)}
-    
+
     # Define empty dictionary that maps agents to actions
     actions = {agent: None for agent in agents}
 
@@ -172,13 +352,6 @@ if __name__ == "__main__":
     # Note the dimensions of the model varies depending on if the parameter sharing algorithm was used or the normal independent 
     # DQN model was used
     if parameter_sharing_model:
-        
-        ###################
-        # TODO: for the coordinated ZSC environemnt, we need each agent to load its own policy, but because we used parameter sharing,
-        # we only have 1 nn for each expert to load. So in this case we need to basically give a copy of the nn to all agents in the env that
-        # are supposed to have this expertise
-        ###################
-
         # Define the shape of the observation space depending on if we're using a global observation or not
         # Regardless, we need to add an array of length num_agents to the observation to account for one hot encoding
         eg_agent = agents[0]
@@ -205,6 +378,7 @@ if __name__ == "__main__":
 
             nn_file = "{}/{}.pt".format(agent_specific_nn_dir, analysis_steps)
             
+            print(f" > Loading policy file: {nn_file} for agent: {agent}")
             actor_network[agent].load_state_dict(torch.load(nn_file))
 
     # Else the agents were trained using normal independent DQN so each agent gets its own Q-network model
@@ -218,6 +392,7 @@ if __name__ == "__main__":
             
             actor_network[agent] = Actor(observation_space_shape, action_spaces[agent].n).to(device)
 
+            # Apply the agent mapping
             file_suffix = AGENT_ALIAS_MAP[agent]
             
             if analysis_training_round > -1:
@@ -279,7 +454,7 @@ if __name__ == "__main__":
         # If the simulation is done, print the episode reward and close the env
         if np.prod(list(dones.values())):
 
-            # Accumulated reward of all agents (should always be using "queue")
+            # Accumulated reward of all agents
             system_episode_reward = sum(list(episode_rewards.values())) 
 
             # Accumulated constraint values
@@ -293,8 +468,7 @@ if __name__ == "__main__":
 
             # TODO: add per agent logging, we need to know about the performance of the center agent in terms of it's defined reward
             for agent in agents:
-                print(f"     > Agent {agent} g1: {episode_constraint_1[agent]} g2: {episode_constraint_2[agent]}")
-
+                print(f"      > Agent {agent} episode reward: {episode_rewards[agent]} g1: {episode_constraint_1[agent]} g2: {episode_constraint_2[agent]}")
             break
 
         # If the parameter sharing model was used, we have to add one hot encoding to the observations
@@ -312,26 +486,35 @@ if __name__ == "__main__":
                     onehot[onehot_keys[agent]] = 1.0
                     next_obses[agent] = np.hstack([onehot, next_obses[agent]])
         
-        # Accumulate the total episode reward and max speeds
+        # Accumulate the rewards and constraints
         for agent in agents:
-
             episode_rewards[agent] += rewards[agent]
 
             # The wrapper class needs to be unwrapped for some reason in order to properly access info
-            info = env.unwrapped.env._compute_info()
+            info_unwrapped = env.unwrapped.env._compute_info()
 
             # Get the per-agent number of stopped cars from the info dictionary
-            agent_cars_stopped = info[f'{agent}_stopped']
+            agent_cars_stopped = info_unwrapped[f'{agent}_stopped']
 
-            # Compute g1 metric
+            # Compute g1 metric only if there were cars present in the intersection 
+            # This conforms to the way the avg speed rewards are calculated
             avg_speed_observed_by_agent = next_obses[agent][-2]
-            episode_constraint_1[agent] += CalculateSpeedError(speed=avg_speed_observed_by_agent, 
-                                                               speed_limit=SPEED_LIMIT,
-                                                               lower_speed_limit=SPEED_LIMIT)
+
+            if ((agent_cars_stopped == 0.0) and (avg_speed_observed_by_agent == 0.0)):
                 
-            # g2 metric
+                # No cars and no average speed means there are no cars present in the intersection
+                g1_from_step = 0.0
+
+            else:
+
+                g1_from_step = CalculateSpeedError(speed=avg_speed_observed_by_agent, 
+                                                    speed_limit=SPEED_LIMIT,
+                                                    lower_speed_limit=SPEED_LIMIT)
+
+            episode_constraint_1[agent] += g1_from_step
+
             episode_constraint_2[agent] += agent_cars_stopped
-                
+
         obses = next_obses
 
         # Log values to csv